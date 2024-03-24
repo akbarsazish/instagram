1f1bccaea9e34e34b10e90b5d9c3a08e273e6df0
akbarsazish <sazishaliakbar@gmail.com>
2024-03-24 19:23:09 +0330

    add signup form and validate it

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Button } from '@/components/ui/button';
 import { Navigate, Outlet } from 'react-router-dom';
-
 
 function AuthLayout() {
    const isAuthenticated = false;
@@ -16,6 +14,9 @@ function AuthLayout() {
          <section className='flex flex-1 justify-center items-center flex-col py-y'>
           <Outlet />
         </section>
+        <img src="/assets/images/side-img.svg"
+        alt="slid-image"
+        className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat" />
         </>
       )}
     </>
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,14 +1,55 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import {Form,FormControl,FormDescription,FormField,FormItem,  FormLabel,FormMessage,} from "@/components/ui/form"
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form"
 import { Button } from "@/components/ui/button"
+import { SignupValidation } from "@/lib/validation";
+import { z } from "zod"
+ 
+
 
 function SignupForm() {
+
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
+    defaultValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof SignupValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   return (
     <>
-      <section className="flex flex-1 justify-center items-center flex-col">
-        <Button>Click me</Button>
-      </section>
-      <img src="/assets/images/side-img.svg"
-      alt="slid-image"
-      className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat" />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
      </>
   )
 }
