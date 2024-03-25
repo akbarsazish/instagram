f4a2eabec51cf23b4b32ea3a619e2aa60e254639
akbarsazish <sazishaliakbar@gmail.com>
2024-03-25 11:52:04 +0330

    add function for creating new user

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,8 @@
 import {ID} from "appwrite"
 import { INewUser } from "@/types";
-import { account } from "./config";
+import { account, avatars} from "./config";
+import { appwriteConfig } from "./config";
+
 
 export async function createUserAccount(user:INewUser) {
     try {
@@ -10,9 +12,42 @@ export async function createUserAccount(user:INewUser) {
             user.password,
             user.name,
         );
-        return newAccount;
+
+        if(!newAccount) throw new Error;
+
+        const avatarUrl = await avatars.getInitials( user.name);
+        const newUser = await saveUserToDb({
+            accountId: newAccount.$id,
+            email: newAccount.email,
+            name: newAccount.name,
+            userName: user.username,
+            imageUrl: avatarUrl,
+        });
+
+        return newUser;
+
     }catch (error) {
         console.log(error)
         return error;
+    }
+}
+
+export async function saveUserToDb(user:{
+    accountId: string;
+    email: string;
+    name: string;
+    imageUrl: URL;
+    userName?: string;
+}) {
+  try {
+        const newUser = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            ID.unique(),
+            user,
+        );
+        return newUser;
+    } catch (error) {
+        console.log(error)
     }
 }
